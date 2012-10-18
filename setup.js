1679fcd8a1fd02c03e45ea5937f5001bae62c3e2
Brian Morrow <bmorrow92@gmail.com>
2012-10-18 19:02:21 -0400

    jQuery skeleton

---
+++ setup.js
@@ -0,0 +1,1 @@
+var me = new User('Brian', 'Morrow', 'killerbat00', 'Chapel Hill', 'UNC', '10182012', 'avatar.gif');
---
+++ tBdraft.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$("a").click(function(event){
+		alert("Thanks for visiting!");
+	});
+
+//	alert('Init function');	
+	
+});
