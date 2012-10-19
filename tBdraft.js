79247299459b7c97af18717c6c9b965cc0103eb5
Brian Morrow <bmorrow92@gmail.com>
2012-10-18 22:32:40 -0400

    update/refactor

--- tBdraft.js
+++ tBdraft.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	alert('Init function');	
-		
 	build_user();
 	build_book_lists();
+	build_feed();
 });			
 
 var build_user = function() {
@@ -38,4 +38,19 @@ var build_list = function(type_div) {
 		books.push('<img class="book" src="samplebook.png" alt="book title" title="book title">');
 		type_div.append(books[i]);	
 	}
+}
+
+var build_feed = function() {
+	var feed_div = $('#feed');
+	
+//	var user_div = $('<div id="user1"></div>');
+//	var user2_div = $('<div id="user2"></div>');
+//	var user3_div = $('<div id="user3"></div>');
+//	var user4_div = $('<div id="user4"></div>');
+//	var user5_div = $('<div id="user5"></div>');
+//	var user6_div = $('<div id="user6"></div>');
+//	var user7_div = $('<div id="user7"></div>');
+//
+//	feed_div.append(user_div.append('<p><img class="avatar" src="avatar.gif" alt="username" title="username"> *Username* just joined the bookworm!</p>');
 }	
+
